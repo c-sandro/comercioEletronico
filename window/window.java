e6d4c856db8403d5f936aea7d3d1fd4bbc29216f
giovanni gomes <vannirgomes@gmail.com>
2023-09-24 22:00:22 -0300

    funcionamento de todos os botões de gerenciamento

--- window/window.java
+++ window/window.java
@@ -1,18 +1,37 @@
 //package window;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class Window extends JFrame{
+public class Window extends JFrame implements ActionListener{
 
-    public static void main(String[] args){
-        
+    private static JFrame frame = new JFrame("Gerenciamento de Produtos");
+    private static JTable table;
+    private static DefaultTableModel tableModel;
+    
+    JButton addProductButton = new JButton("Adicionar Produto");
+    JButton editProductButton = new JButton("Editar Produto");
+    JButton deleteProductButton = new JButton("Excluir Produto");
+    JButton backButton = new JButton("Voltar Ao Menu Principal");
+    JButton addClientButton = new JButton("Adicionar Cliente");
+    JButton editClientButton = new JButton("Editar Cliente");
+    JButton deleteClientButton = new JButton("Excluir Cliente");
+    JButton addOrderButton = new JButton("Adicionar Pedido");
+    JButton editOrderButton = new JButton("Editar Pedido");
+    JButton deleteOrderButton = new JButton("Excluir Pedido");
+
+    int userOption = 0;
+
+    public Window(){
         String[] options = new String[]{
             "produto",
             "cliente",
             "pedidos",
             "sair"
         };
-
-        int userOption = 0;
 
         while(userOption != 3){
             userOption = JOptionPane.showOptionDialog(
@@ -25,21 +44,163 @@ public class Window extends JFrame{
                 options, 
                 0);
                 
-                if(userOption == 0){
-                    ProductFrame.productFrame();
-                    break;
-                } 
-                else if (userOption == 1) {
-                    ClientFrame.clientFrame();
-                    break;
-                } 
-                else if (userOption == 2) {
-                    OrderFrame.orderFrame();
-                    break;
-                } 
-                else {
-                    JOptionPane.showMessageDialog(null, "Programa Encerrado");
+            if(userOption == 0){
+                ProductFrame();
+                break;
+            } 
+            else if (userOption == 1) {
+                ClientFrame();
+                break;
+            } 
+            else if (userOption == 2) {
+                OrderFrame();
+                break;
+            } 
+            else {
+                JOptionPane.showMessageDialog(null, "Programa Encerrado");
+            }
+        }
+    }
+
+
+    public JFrame ProductFrame(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        frame.getContentPane().add(mainPanel);
+        frame.setLocation(480, 170);
+
+    //###TABELA###
+        tableModel = new DefaultTableModel();
+        table = new JTable(tableModel);
+        tableModel.addColumn("Nome");
+        tableModel.addColumn("Preço");
+        tableModel.addColumn("Quantidade");
+        tableModel.addColumn("ID");
+        JScrollPane scrollPane = new JScrollPane(table);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+    //###BOTÕES###
+        JPanel buttonPanel = new JPanel();
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        addProductButton.setBackground(Color.lightGray);
+        editProductButton.setBackground(Color.lightGray);
+        deleteProductButton.setBackground(Color.lightGray);
+        backButton.setBackground(Color.lightGray);
+        buttonPanel.setBackground(Color.gray);
+
+        buttonPanel.add(addProductButton);
+        buttonPanel.add(editProductButton);
+        buttonPanel.add(deleteProductButton);
+        buttonPanel.add(backButton);
+        addProductButton.addActionListener(this);
+        editProductButton.addActionListener(this);
+        deleteProductButton.addActionListener(this);
+        backButton.addActionListener(this);
+
+        frame.pack();
+        frame.setVisible(true);
+        return frame;
+    }
+    
+    public JFrame OrderFrame(){
+        frame = new JFrame("Gerenciamento de Pedidos");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(mainPanel);
+        frame.setLocation(480, 170);
+
+        tableModel = new DefaultTableModel();
+        table = new JTable(tableModel);
+        tableModel.addColumn("ID");
+        tableModel.addColumn("Pedido");
+        tableModel.addColumn("Cliente");
+        tableModel.addColumn("Valor");
+        JScrollPane scrollPane = new JScrollPane(table);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+
+        buttonPanel.add(addOrderButton);
+        buttonPanel.add(editOrderButton);
+        buttonPanel.add(deleteOrderButton);
+        buttonPanel.add(backButton);
+        addOrderButton.addActionListener(this);
+        editOrderButton.addActionListener(this);
+        deleteOrderButton.addActionListener(this);
+        backButton.addActionListener(this);
+
+        frame.pack();
+        frame.setVisible(true);
+        return frame;
+    }
+
+    public JFrame ClientFrame(){
+        frame = new JFrame("Gerenciamento de Clientes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(mainPanel);
+        frame.setLocation(480, 170);
+
+        tableModel = new DefaultTableModel();
+        table = new JTable(tableModel);
+        tableModel.addColumn("Nome");
+        tableModel.addColumn("Endereço");
+        tableModel.addColumn("CPF");
+        tableModel.addColumn("Saldo");
+        JScrollPane scrollPane = new JScrollPane(table);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        buttonPanel.add(addClientButton);
+        buttonPanel.add(editClientButton);
+        buttonPanel.add(deleteClientButton);
+        buttonPanel.add(backButton);
+        addClientButton.addActionListener(this);
+        editClientButton.addActionListener(this);
+        deleteClientButton.addActionListener(this);
+        backButton.addActionListener(this);
+
+        frame.pack();
+        frame.setVisible(true);
+        return frame;
+    }
+
+    
+            public static void main(String[] args){
+                new Window();
+            }
+
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==addProductButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste adicionar produto");
+                } else if(e.getSource()==editProductButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste editar produto");
+                } else if(e.getSource()==deleteProductButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste deletar produto");
+                } else if(e.getSource()==backButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste voltar");
+                } else if(e.getSource()==addClientButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste adicionar cliente");
+                } else if(e.getSource()==editClientButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste editar cliente");
+                } else if(e.getSource()==deleteClientButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste deletar cliente");
+                }else if(e.getSource()==addOrderButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste adicionar pedido");
+                } else if(e.getSource()==editOrderButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste editar pedido");
+                } else if(e.getSource()==deleteOrderButton){
+                    JOptionPane.showMessageDialog(rootPane, "teste deletar pedido");
                 }
             }
-    }
 }
